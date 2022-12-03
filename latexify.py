f22a2625ff9d506ab2480166136bb08614bf9aec
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-12-04 01:43:56 +0200

    Add latexify.py script
    Some utilities to make the conversion less painful

---
+++ latexify.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+import re
+import subprocess
+
+def cite(groups):
+    ref = groups.group(1)
+    
+    is_range_of_refs = '-' in ref
+    if is_range_of_refs:
+        ref = ref.split('-')
+        ref = range(int(ref[0]), int(ref[1])+1)
+        ref = ','.join(f'bib{i}' for i in ref)
+    else:
+        ref = f'bib{ref}'
+
+    return '~\\cite{' + ref + '}'
+
+def latexify(txt):
+    txt = re.sub(r' \[([0-9-,]+)\]', cite, txt)
+    return txt.replace('%', '\%')
+
+
+
+def getClipboardData():
+    p = subprocess.Popen(['xclip','-selection', 'clipboard', '-o'], stdout=subprocess.PIPE)
+    p.wait()
+    data = p.stdout.read()
+    return data.decode('utf-8')
+
+def setClipboardData(data):
+    p = subprocess.Popen(['xclip','-selection','clipboard'], stdin=subprocess.PIPE)
+    p.stdin.write(data)
+    p.stdin.close()
+    p.wait()
+
+def references():
+    with open('references.txt') as fp:
+        refs = fp.readlines()
+        assert len(refs) == 55
+    s = ''
+    for i, ref in enumerate(refs):
+        s += '\\bibitem{bib' + str(i+1) + '}\n' + "ref" + "\n\\newblock{info} \n\n"
+    print(s)
+
+if __name__ == '__main__':
+    latexified = latexify(getClipboardData())
+    # setClipboardData(byte(latexified))
+    print(latexified)
