0cec5b3f91b9e8764cb6dab86c49768041114d48
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-12-04 21:53:34 +0200

    Compress tiff files

--- convert_tiff2png.bash
+++ convert_tiff2png.bash
@@ -3,4 +3,3 @@
 # Usage: convert_tiff2png.bash <tiff_dir> <png_dir>
 # Example: convert_tiff2png.bash convert_tiff2png.bash figures png
 find $1 -name "*.tiff" -execdir convert {} $2/$(basename {}).png \;
-# find $1 -name "*.tiff" -exec basename \{} \;
