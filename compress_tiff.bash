923bffd0fabf09553e5a744489117668e6707243
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-12-11 15:24:58 +0200

    Initial Design

--- compress_tiff.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Compress all tiff files in a directory
-# Usage: compress_tiff.bash <tiff_dir>
-# Example: compress_tiff.bash figures
-find $1 -maxdepth 1 -name "*.tiff" -execdir  convert {} -compress lzw {} \;
--- convert_tiff2png.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Convert all tiff files in a directory to png files into a new directory
-# Usage: convert_tiff2png.bash <tiff_dir> <png_dir>
-# Example: convert_tiff2png.bash convert_tiff2png.bash figures png
-find $1 -name "*.tiff" -execdir convert {} $2/$(basename {}).png \;
